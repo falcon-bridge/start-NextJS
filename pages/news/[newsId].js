2acf85a2bc8533bf76bab0bac5cc0891f41b2e0b
Aayush <falcon.bridge2000@gmail.com>
2022-02-07 05:08:11 +0530

    extracted dynamic parameter values

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,4 +1,10 @@
+import { useRouter } from "next/router";
+
 const DetailPage = () => {
+  const router = useRouter();
+
+  console.log(router.query.newsId);
+
   return <h1>The Detail page</h1>;
 };
 
