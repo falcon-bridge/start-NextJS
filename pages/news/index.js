c0f6df1fd1684639218f0ec72c1ba367402f2e8b
Aayush <falcon.bridge2000@gmail.com>
2022-02-07 05:16:42 +0530

    Linked pages using component provided by next package

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,18 @@
+import { Fragment } from "react";
+
+import Link from "next//link";
+
 const NewsPage = () => {
-  return <h1>The News page</h1>;
+  return (
+    <Fragment>
+      <h1>The News page</h1>
+      <ul>
+        <li>
+          <Link href="/news/detail">See details</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default NewsPage;
